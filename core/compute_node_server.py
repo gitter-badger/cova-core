e848fcc10ae67e88a6e355b5425af4f915042609
Tarik Moon <tarik@tarikmoon.com>
2018-12-01 03:06:37 +0600

    compute node main script moved to top level

---
+++ core/compute_node_server.py
@@ -0,0 +1,6 @@
+import sys
+from computenode import computer
+
+if __name__ == "__main__":
+    # TODO: check argv
+    computer.run(sys.argv[1])
--- core/computenode/compute_node_server.py
+++
@@ -1,6 +0,0 @@
-from covavm import *
-from gossipnet import computer
-
-if __name__ == "__main__":
-
-    computer.run(sys.argv[1])
