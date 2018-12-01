24401f326d36578c84609245e16213dd528496ca
Tarik Moon <tarik@tarikmoon.com>
2018-12-02 01:58:31 +0600

    exception on run server on only cred read

--- core/run_server_node.py
+++ core/run_server_node.py
@@ -9,19 +9,23 @@ def get_credentials(cred_path):
 
 
 if __name__ == "__main__":
+    cred = None
     try:
         if len(sys.argv) == 2:
             cred = get_credentials(sys.argv[1])
         else:
             cred = get_credentials(DEFAULT_CRED_PATH)
-
-        if cred["router"]:
-            from routingnode import router
-            router.run(cred["id"], cred)
-        else:
-            from computenode import computer
-            computer.run(cred["eth_cred"]["address"], cred["public_ip"])
-
     except Exception as e:
+        print "FAILED TO GET CREDENTIALS"
         raise e
+        sys.exit()
+
+    if cred["router"]:
+        from routingnode import router
+        router.run(cred["id"], cred)
+    else:
+        from computenode import computer
+        computer.run(cred["eth_cred"]["address"], cred["public_ip"])
+
+    
     
